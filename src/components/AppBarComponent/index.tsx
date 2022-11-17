ec29c1ea994bf707a18e7dd8f730c27b2ba8e516
Qurban Ahmad <ahmad76qurban@gmail.com>
2022-11-18 00:25:16 +0530

    feat: add rest of the links

--- src/components/AppBarComponent/index.tsx
+++ src/components/AppBarComponent/index.tsx
@@ -1,40 +1,139 @@
-import { AppBar, Toolbar, Avatar, Typography, Link, Grid } from "@mui/material";
-import { yellow } from "@mui/material/colors";
+import {
+  AppBar,
+  Toolbar,
+  Avatar,
+  Typography,
+  Link,
+  Grid,
+  Stack,
+} from "@mui/material";
+import { yellow, grey } from "@mui/material/colors";
 import { useState } from "react";
 import type { LinkProps } from "@mui/material";
 
-export function CustomAppBar() {
-  const [nameColor, setNameColor] = useState("black");
+const Links = {
+  Home: "home",
+  Contact: "contact",
+  Projects: "projects",
+  Title: "title",
+  About: "about",
+} as const;
 
-  const onMouseMoveHandler: LinkProps["onMouseMove"] = () => {
-    setNameColor(yellow[600]);
+type Links = typeof Links[keyof typeof Links];
+
+export function CustomAppBar() {
+  const [isHover, setIsHover] = useState<Record<Links, boolean>>({
+    title: false,
+    home: false,
+    contact: false,
+    projects: false,
+    about: false,
+  });
+
+  const onMouseMoveHandler: LinkProps["onMouseMove"] = (event) => {
+    const dataName = event.currentTarget.getAttribute("data-name");
+    setIsHover((prev) => ({
+      ...prev,
+      [dataName ?? ""]: true,
+    }));
   };
 
-  const onMouseLeaveHandler: LinkProps["onMouseLeave"] = () => {
-    setNameColor("black");
+  const onMouseLeaveHandler: LinkProps["onMouseLeave"] = (event) => {
+    const dataName = event.currentTarget.getAttribute("data-name");
+    setIsHover((prev) => ({
+      ...prev,
+      [dataName ?? ""]: false,
+    }));
   };
 
   return (
     <AppBar color="transparent">
       <Toolbar>
-        <Link
-          href="https://github.com/qur786"
-          underline="none"
-          onMouseMove={onMouseMoveHandler}
-          onMouseLeave={onMouseLeaveHandler}
-        >
-          <Grid container spacing={2} alignItems="center">
+        <Grid container>
+          <Grid container item xs={6}>
             <Grid item>
-              {/* TODO: replace it with image */}
-              <Avatar sx={{ bgcolor: yellow[600] }}>Q</Avatar>
-            </Grid>
-            <Grid item>
-              <Typography sx={{ fontWeight: "bolder", color: nameColor }}>
-                QURBAN AHMAD
-              </Typography>
+              <Link
+                href="https://github.com/qur786"
+                underline="none"
+                onMouseMove={onMouseMoveHandler}
+                onMouseLeave={onMouseLeaveHandler}
+                color={isHover.title === true ? yellow[600] : grey[900]}
+                data-name={Links.Title}
+              >
+                <Stack direction="row" alignItems="center" spacing={2}>
+                  <Avatar sx={{ bgcolor: yellow[600] }}>Q</Avatar>
+                  <Typography sx={{ fontWeight: "bolder" }}>
+                    QURBAN AHMAD
+                  </Typography>
+                </Stack>
+              </Link>
             </Grid>
           </Grid>
-        </Link>
+          <Grid
+            container
+            item
+            xs={6}
+            alignItems="center"
+            justifyContent="space-around"
+          >
+            <Grid item>
+              <Link
+                href="#"
+                underline="none"
+                variant="button"
+                data-name={Links.Home}
+                color={isHover.home === true ? yellow[600] : grey[900]}
+                onMouseMove={onMouseMoveHandler}
+                onMouseLeave={onMouseLeaveHandler}
+                sx={{ fontWeight: "bolder" }}
+              >
+                Home
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link
+                href="#"
+                underline="none"
+                variant="button"
+                data-name={Links.About}
+                color={isHover.about === true ? yellow[600] : grey[900]}
+                onMouseMove={onMouseMoveHandler}
+                onMouseLeave={onMouseLeaveHandler}
+                sx={{ fontWeight: "bolder" }}
+              >
+                About
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link
+                href="#"
+                underline="none"
+                variant="button"
+                data-name={Links.Projects}
+                color={isHover.projects === true ? yellow[600] : grey[900]}
+                onMouseMove={onMouseMoveHandler}
+                onMouseLeave={onMouseLeaveHandler}
+                sx={{ fontWeight: "bolder" }}
+              >
+                Projects
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link
+                href="#"
+                underline="none"
+                variant="button"
+                data-name={Links.Contact}
+                color={isHover.contact === true ? yellow[600] : grey[900]}
+                onMouseMove={onMouseMoveHandler}
+                onMouseLeave={onMouseLeaveHandler}
+                sx={{ fontWeight: "bolder" }}
+              >
+                Contact
+              </Link>
+            </Grid>
+          </Grid>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
