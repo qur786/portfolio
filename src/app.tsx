f3ab98ec44be45fd29aa38cf2a02a823355c3033
Qurban Ahmad <ahmad76qurban@gmail.com>
2022-11-21 23:41:39 +0530

    feat: add initial text to app

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,38 @@
-import { useMediaQuery, useTheme } from "@mui/material";
+import { Grid, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { CustomAppBar } from "./components/AppBarComponent";
 import { MobileCustomAppBar } from "./components/MobileAppBarComponent";
 
-export function App() {
+export function App(): JSX.Element {
   const theme = useTheme();
   const mediaQuery = useMediaQuery(theme.breakpoints.up("sm"));
-  return mediaQuery === true ? <CustomAppBar /> : <MobileCustomAppBar />;
+  return (
+    <Grid container direction="column">
+      <Grid item marginBottom="12vh">
+        {mediaQuery === true ? <CustomAppBar /> : <MobileCustomAppBar />}
+      </Grid>
+      <Grid
+        container
+        item
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        height="80vh"
+      >
+        <Grid item>
+          <Typography variant="h3" align="center" sx={{ fontWeight: "bolder" }}>
+            Hey! I am Qurban Ahmad
+          </Typography>
+        </Grid>
+        <Grid container item justifyContent="center">
+          <Grid item xs={12} sm={8}>
+            <Typography variant="h6" align="center">
+              A Frontend focused Web Developer building the Frontend of Websites
+              and Web Applications that leads to the success of the overall
+              product.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
