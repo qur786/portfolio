49dfe5321df64cac5a53d2eb6021693105b7ade3
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-14 20:58:21 +0530

    refactor: add simple header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 export function Header(): JSX.Element {
   return (
-    <header className="h-16 bg-black">{/* TODO: add logo, title and links */}</header>
+    <header className="h-16 bg-black">
+      {/* TODO: add logo, title and links */}
+    </header>
   );
 }
