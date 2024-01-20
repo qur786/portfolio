9fe55f6e77e85b4a4785205085b390de044d4c5d
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-20 19:58:31 +0530

    feat: added entry point

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 export function App(): JSX.Element {
   return (
     <main>
-      <p>Qurban</p>
+      <div className="h-screen flex justify-center items-center flex-col">
+        <p className="text-5xl">
+          Hello, I'm <b className="text-red-600">Qurban</b>
+        </p>
+        <p className="text-5xl">
+          I am a <b className="text-green-600">MERN</b> stack developer
+        </p>
+      </div>
     </main>
   );
 }
