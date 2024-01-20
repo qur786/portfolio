6ce77eb77dfa91fc3bf51d1b5af1b1ebb0411f6c
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-20 21:48:48 +0530

    feat: added particles background

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,76 @@
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import { useEffect } from "react";
+
 export function App(): JSX.Element {
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    });
+  }, []);
+
   return (
     <main>
+      <Particles
+        id="tsparticles"
+        particlesLoaded={async (c) => console.log(c)}
+        options={{
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+              },
+              resize: {
+                enable: true,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#E71C23",
+            },
+            links: {
+              color: "#B83227",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 1,
+              straight: false,
+            },
+            number: {
+              value: 200,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
       <div className="h-screen flex justify-center items-center flex-col">
         <p className="text-5xl">
           Hello, I'm <b className="text-red-600">Qurban</b>
