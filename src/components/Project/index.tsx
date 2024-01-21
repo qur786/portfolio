e3c80e504ea6b72926fc23f69bfffdbc985a994a
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-21 17:14:19 +0530

    feat: added project card proper styling

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -1,3 +1,31 @@
-export function Project(): JSX.Element {
-  return <div>project</div>;
+interface ProjectProps {
+  image?: string;
+  imageAlt?: string;
+  title: string;
+  description: string;
+  deployedLink?: string;
+  githubLink: string;
+}
+
+export function Project({
+  description,
+  githubLink,
+  title,
+  deployedLink,
+  image,
+  imageAlt,
+}: ProjectProps): JSX.Element {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-gray-800">
+      <img className="w-full h-60 object-cover" src={image} alt={imageAlt} />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{title}</div>
+        <p className="text-base">{description}</p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        {githubLink}
+        {deployedLink}
+      </div>
+    </div>
+  );
 }
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -4,8 +4,8 @@ export function Projects(): JSX.Element {
   return (
     <div id="projects" className="h-screen px-4 py-8 ">
       <h2 className="text-3xl text-[#FF020A] font-bold">Projects</h2>
-      <div>
-        <Project />
+      <div className="grid grid-cols-3 gap-4 py-6">
+        <Project title={""} description={""} githubLink={""} />
       </div>
     </div>
   );
