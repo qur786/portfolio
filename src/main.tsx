9f69b5d761c6328d9e3432ac716bcee0e8f27330
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-21 13:35:19 +0530

    build: self host roboto font

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
+import "@fontsource/roboto/100.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/900.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
