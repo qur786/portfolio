ec10d0cf9b3594ae60047a3389fa0620c9dfee53
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-21 19:36:13 +0530

    feat: added 3 projects

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -8,6 +8,7 @@ interface ProjectProps {
   deployedLink?: string;
   githubLink: string;
   stacks: string[];
+  imageClipPosition?: string;
 }
 
 export function Project({
@@ -17,16 +18,17 @@ export function Project({
   image,
   imageAlt,
   stacks,
+  imageClipPosition = "0px_25%",
 }: ProjectProps): JSX.Element {
   return (
-    <div className="max-w-[280px] h-[440px] rounded-lg bg-[#222222] ">
+    <div className="max-w-[280px] h-[440px] rounded-lg bg-[#222222]">
       <img
-        className="w-full h-60 object-cover rounded-tl-lg rounded-tr-lg object-[0px_25%] "
+        className={`w-full h-60 object-cover rounded-tl-lg rounded-tr-lg object-[${imageClipPosition}]`}
         src={image}
         alt={imageAlt}
       />
-      <div className="h-10 flex flex-col gap-4">
-        <div className="px-6 py-2 font-bold text-xl text-center">{title}</div>
+      <div className="h-48 flex flex-col justify-between">
+        <div className="px-12 py-2 font-bold text-xl text-center">{title}</div>
         {stacks.length > 0 ? (
           <div className="flex flex-row justify-start flex-wrap px-2 gap-2">
             {stacks.slice(0, 6).map((stack) => (
@@ -36,7 +38,7 @@ export function Project({
             ))}
           </div>
         ) : undefined}
-        <div className="px-6 flex flex-row justify-between ">
+        <div className="px-6 flex flex-row justify-between">
           <a
             href={deployedLink}
             title="Live"
