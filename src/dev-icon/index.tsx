cec4966214cbdd19d387d166570ab5e6fdbd35d9
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-22 00:05:57 +0530

    refactor: update dev icon size and their num per column

--- src/dev-icon/index.tsx
+++ src/dev-icon/index.tsx
@@ -8,7 +8,7 @@ interface DevIconProps {
 export function DevIcon({ name, colored = true }: DevIconProps): JSX.Element {
   return (
     <i
-      className={`${name} ${colored === false ? "" : "colored"} text-8xl flex justify-center rounded-md bg-gray-900 p-2`}
+      className={`${name} ${colored === false ? "" : "colored"} text-6xl flex justify-center rounded-md bg-gray-900 p-2`}
     ></i>
   );
 }
