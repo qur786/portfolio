ad151a4bc614555fc3c7204c2a312f314ce60235
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-21 16:22:33 +0530

    build: add port server 3000

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   base: "/portfolio/", // Ref: https://vitejs.dev/guide/static-deploy#github-pages
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
 });
