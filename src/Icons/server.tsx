f33d2115df61903cfa5a696be0ff074a490b2b87
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-21 18:39:39 +0530

    feat: added github and deployed links via icons

---
+++ src/Icons/server.tsx
@@ -0,0 +1,24 @@
+export function ServerIcon(): JSX.Element {
+  return (
+    <svg
+      enableBackground="new 0 0 32 32"
+      className="h-8"
+      height="32px"
+      id="svg2"
+      version="1.1"
+      viewBox="0 0 32 32"
+      xmlSpace="preserve"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="background">
+        <rect fill="none" height="32" width="32" />
+      </g>
+      <g id="server">
+        <path
+          fill="white"
+          d="M30,10V2H2v8h2v2H2v8h2v2H2v8h28v-8h-2v-2h2v-8h-2v-2H30z M4,6h4v2H4V6z M4,16h4v2H4V16z M8,28H4v-2h4V28z M26,22H6v-2h20   V22z M26,12H6v-2h20V12z"
+        />
+      </g>
+    </svg>
+  );
+}
--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -1,3 +1,6 @@
+import { GithubCircleIcon } from "../../Icons/github-circle";
+import { ServerIcon } from "../../Icons/server";
+
 interface ProjectProps {
   image: string;
   imageAlt?: string;
@@ -14,16 +17,22 @@ export function Project({
   imageAlt,
 }: ProjectProps): JSX.Element {
   return (
-    <div className="max-w-[280px] h-[440px] rounded overflow-hidden bg-[#222222]">
+    <div className="max-w-[280px] h-[440px] rounded-lg bg-[#222222] ">
       <img
-        className="w-full h-60 object-cover object-[5px_25%]"
+        className="w-full h-60 object-cover rounded-tl-lg rounded-tr-lg object-[0px_25%] "
         src={image}
         alt={imageAlt}
       />
-      <div className="px-6 py-4 font-bold text-xl">{title}</div>
-      <div className="px-6">
-        {githubLink}
-        {deployedLink}
+      <div className="h-10 flex flex-col gap-14">
+        <div className="px-6 py-4 font-bold text-xl ">{title}</div>
+        <div className="px-6 flex flex-row justify-between ">
+          <a href={deployedLink}>
+            <ServerIcon />
+          </a>
+          <a href={githubLink}>
+            <GithubCircleIcon />
+          </a>
+        </div>
       </div>
     </div>
   );
