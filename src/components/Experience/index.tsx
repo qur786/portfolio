030efa2ddb1d4e342518a6d4f9efb126ef373b9d
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-21 15:16:35 +0530

    feat: added front end developer intern experience

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -31,10 +31,22 @@ export function Experience(): JSX.Element {
           company="Manufac Analytics Pvt Ltd"
           role="Front End Developer"
           work={[
-            "Engineered diverse projects: web apps, single-page apps, and Electron apps. Innovations include tracking user activity with Python and Electron, and real-time IoT tracking with AWS IoT, Geofencing, Lambda, and SNS",
+            "Engineered diverse projects: web apps, single-page apps, and Electron apps. Innovations include tracking user activity with Python and Electron, and real-time IoT tracking with AWS IoT, Geofencing, Lambda, and SNS.",
             "Improved website performance by optimizing Lighthouse scores, achieving a 90+% score in Lighthouse.",
             "Enhanced UI with mobile-responsive components, charts, and forms in React. Achieved a 30% improvement in UI responsiveness.",
             "Developed and debugged the company's core product, API. Improving reliability by 10%.",
+          ]}
+        />
+        <ExperienceItem
+          date="July 2021 - Jan 2022"
+          icon={<JobIcon />}
+          location={{ city: "Gurugram", country: "India" }}
+          company="Manufac Analytics Pvt Ltd"
+          role="Front End Developer Intern"
+          work={[
+            "Spearheaded front-end tasks using React, leading to a 15% improvement in UI enhancements in the company's core SaaS product.",
+            "Helped team in API integration in the Company's web app to bring the new features. Improving app functionality by 20%.",
+            "Collaborated with team to integrate PayPal payment API in the company's web app.",
           ]}
         />
         <VerticalTimelineElement
