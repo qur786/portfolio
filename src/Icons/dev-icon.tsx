b3bd6be655850e07159072f94f05b33644916421
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-21 13:24:36 +0530

    feat: added more skills

--- src/Icons/dev-icon.tsx
+++ src/Icons/dev-icon.tsx
@@ -1,11 +1,12 @@
 interface DevIconProps {
   name: string;
+  colored?: boolean;
 }
 
-export function DevIcon({ name }: DevIconProps): JSX.Element {
+export function DevIcon({ name, colored }: DevIconProps): JSX.Element {
   return (
     <i
-      className={`${name} colored text-8xl flex justify-center rounded-md bg-gray-900 p-2`}
+      className={`${name} ${colored === false ? "" : "colored"} text-8xl flex justify-center rounded-md bg-gray-900 p-2`}
     ></i>
   );
 }
--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -6,9 +6,21 @@ export function Skills(): JSX.Element {
       <h2 className="text-3xl text-[#FF020A] font-bold">My Skills</h2>
       <div className="grid grid-cols-8 gap-4">
         <DevIcon name="devicon-mongodb-plain-wordmark" />
-        <DevIcon name="devicon-express-original-wordmark" />
+        <DevIcon name="devicon-express-original-wordmark" colored={false} />
         <DevIcon name="devicon-react-original-wordmark" />
         <DevIcon name="devicon-nodejs-plain-wordmark" />
+        <DevIcon name="devicon-redux-original" />
+        <DevIcon name="devicon-nextjs-plain-wordmark" colored={false} />
+        <DevIcon name="devicon-html5-plain-wordmark" />
+        <DevIcon name="devicon-css3-plain-wordmark" />
+        <DevIcon name="devicon-tailwindcss-original-wordmark" />
+        <DevIcon name="devicon-sass-original" />
+        <DevIcon name="devicon-javascript-plain" />
+        <DevIcon name="devicon-typescript-plain" />
+        <DevIcon name="devicon-materialui-plain" />
+        <DevIcon name="devicon-d3js-plain" />
+        <DevIcon name="devicon-graphql-plain-wordmark" />
+        <DevIcon name="devicon-socketio-original-wordmark" colored={false} />
       </div>
     </div>
   );
