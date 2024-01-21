013020b17c73631ffd0fd9f6b96abe7cd1aaffca
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-21 20:04:02 +0530

    feat: added hook to detect mobile screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Introduction } from "./components/Introduction";
 import { SocialSidebar } from "./components/SocialSidebar";
 import { About } from "./components/About";
 import { Skills } from "./components/Skills";
 import { Experience } from "./components/Experience";
 import { Projects } from "./components/Projects";
+import { useMobile } from "./hooks";
 
 export function App(): JSX.Element {
   const [isParticlesEngineLoaded, setIsParticlesEngineLoaded] = useState(false);
+  const mainRef = useRef<HTMLDivElement>(null);
+  const isMobile = useMobile(mainRef);
+  console.log(isMobile); // Provide it in the react context
 
   // this should be run only once per application lifetime
   useEffect(() => {
@@ -21,7 +25,7 @@ export function App(): JSX.Element {
   }, []);
 
   return (
-    <main className="select-none">
+    <main className="select-none" ref={mainRef}>
       <Introduction isParticlesEngineLoaded={isParticlesEngineLoaded} />
       <SocialSidebar />
       <div className="px-16">
