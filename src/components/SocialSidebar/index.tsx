3bd4029d6c25845420dc64022ef076e63cc6dcd6
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-22 12:14:41 +0530

    feat: added animation to social links

--- src/components/SocialSidebar/index.tsx
+++ src/components/SocialSidebar/index.tsx
@@ -1,3 +1,4 @@
+import { Fade, Slide } from "react-awesome-reveal";
 import { GithubIcon } from "../../Icons/github";
 import { GmailIcon } from "../../Icons/gmail";
 import { LinkedInIcon } from "../../Icons/linked-in";
@@ -8,31 +9,33 @@ export function SocialSidebar(): JSX.Element {
   return (
     <>
       <div className="fixed bottom-0 left-0 p-4 flex flex-col gap-4">
-        <a href="https://www.linkedin.com/in/qurban-ahmad-qur786/">
-          <LinkedInIcon />
-        </a>
-        <a href="https://twitter.com/fantasticnerd67">
-          <TwitterIcon />
-        </a>
-        <a href="https://github.com/qur786">
-          <GithubIcon />
-        </a>
-        <a href="https://wa.me/+916204789892">
-          <WhatsappIcon />
-        </a>
-        <a href="mailto:ahmad76qurban@gmail.com">
-          <GmailIcon />
-        </a>
+        <Fade cascade damping={0.5} triggerOnce>
+          <a href="https://www.linkedin.com/in/qurban-ahmad-qur786/">
+            <LinkedInIcon />
+          </a>
+          <a href="https://twitter.com/fantasticnerd67">
+            <TwitterIcon />
+          </a>
+          <a href="https://github.com/qur786">
+            <GithubIcon />
+          </a>
+          <a href="https://wa.me/+916204789892">
+            <WhatsappIcon />
+          </a>
+          <a href="mailto:ahmad76qurban@gmail.com">
+            <GmailIcon />
+          </a>
+        </Fade>
       </div>
-      <div>
+      <Slide triggerOnce delay={5 * 500}>
         <a
-          href="mailto:ahmad76qurban@gmail.com"
+          href="mailto:ahmad76qurban@gbouncemail.com"
           className="fixed right-0 top-2 hover:text-red-600"
           style={{ writingMode: "vertical-rl" }}
         >
           ahmad76qurban@gmail.com
         </a>
-      </div>
+      </Slide>
     </>
   );
 }
