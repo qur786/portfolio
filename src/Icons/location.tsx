a7e8aca96d7eb7e0ed0fc7853754f1bff4995167
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-25 22:47:11 +0530

    refactor: reduce location icon height and width

--- src/Icons/location.tsx
+++ src/Icons/location.tsx
@@ -6,9 +6,9 @@ export function LocationIcon({
   return (
     <svg
       className={className}
-      height="24"
+      height="18"
       viewBox="0 0 48 48"
-      width="24"
+      width="18"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
