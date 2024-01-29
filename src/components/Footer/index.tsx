66213e19c0335a0e80333b633c3252fd110dff25
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-29 20:25:07 +0530

    feat: added back to top smooth functionality

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,16 +1,26 @@
 import { CallIcon } from "../../Icons/call";
 import { MessageIcon } from "../../Icons/message";
 import { UpArrowIcon } from "../../Icons/up-arrow";
-import { FooterButton } from "../FooterButton";
+import { FooterButton, FooterButtonProps } from "../FooterButton";
 
 export function Footer(): JSX.Element {
+  const handleBackToTopClick: FooterButtonProps["onClick"] = () => {
+    window.scroll({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
   return (
     <div className="h-72 pt-8">
       <h5 className="text-primary font-semibold text-2xl static bottom-0 text-center py-12">
         Thanks for visiting!
       </h5>
       <div className="flex flex-row justify-center gap-8">
-        <FooterButton title="Back to top" icon={UpArrowIcon} />
+        <FooterButton
+          title="Back to top"
+          icon={UpArrowIcon}
+          onClick={handleBackToTopClick}
+        />
         <FooterButton title="Send message" icon={MessageIcon} />
         <FooterButton title="Give me a call" icon={CallIcon} />
       </div>
--- src/components/FooterButton/index.tsx
+++ src/components/FooterButton/index.tsx
@@ -1,17 +1,22 @@
-import type { ComponentType } from "react";
+import type { ComponentType, MouseEventHandler } from "react";
 import { IconProps } from "../../Icons/utils";
 
-interface FooterButtonProps {
+export interface FooterButtonProps {
   title: string;
   icon: ComponentType<IconProps>;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 export function FooterButton({
   title,
   icon: Icon,
+  onClick,
 }: FooterButtonProps): JSX.Element {
   return (
-    <button className="hover:text-primary hover:scale-110 transition-[color,transform] font-bold text-gray-500">
+    <button
+      onClick={onClick}
+      className="hover:text-primary hover:scale-110 transition-[color,transform] font-bold text-gray-500"
+    >
       <Icon className="fill-primary h-6 inline" />
       <p>{title}</p>
     </button>
