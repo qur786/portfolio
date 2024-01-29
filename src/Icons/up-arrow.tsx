fcab3bf9859757a96a9e319601329190d16f7ba2
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-29 20:20:32 +0530

    refactor: added new up arrow icon

--- src/Icons/up-arrow.tsx
+++ src/Icons/up-arrow.tsx
@@ -3,16 +3,13 @@ import { IconProps } from "./utils";
 export function UpArrowIcon({ className }: IconProps): JSX.Element {
   return (
     <svg
-      height="16"
-      viewBox="0 0 16 16"
-      width="16"
+      version="1.1"
+      viewBox="0 0 512 512"
       xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
       className={className}
     >
-      <polygon
-        fillRule="evenodd"
-        points="9 5.414 9 14 7 14 7 5.414 3.707 8.707 2.293 7.293 8 1.586 13.707 7.293 12.293 8.707"
-      />
+      <path d="M128.4,189.3L233.4,89c5.8-6,13.7-9,22.4-9c8.7,0,16.5,3,22.4,9l105.4,100.3c12.5,11.9,12.5,31.3,0,43.2  c-12.5,11.9-32.7,11.9-45.2,0L288,184.4v217c0,16.9-14.3,30.6-32,30.6c-17.7,0-32-13.7-32-30.6v-217l-50.4,48.2  c-12.5,11.9-32.7,11.9-45.2,0C115.9,220.6,115.9,201.3,128.4,189.3z" />
     </svg>
   );
 }
