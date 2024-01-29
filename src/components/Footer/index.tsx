ae5efa8e34fe6c4d91346526575e1c0de69806e3
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-29 21:12:51 +0530

    feat: add slope to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -11,22 +11,28 @@ export function Footer(): JSX.Element {
     });
   };
   return (
-    <div className="h-72 pt-8">
-      <h5 className="text-primary font-semibold text-2xl static bottom-0 text-center py-12">
-        Thanks for visiting!
-      </h5>
-      <div className="flex flex-row justify-center gap-8">
-        <FooterButton
-          title="Back to top"
-          icon={UpArrowIcon}
-          onClick={handleBackToTopClick}
-        />
-        <FooterButton title="Send message" icon={MessageIcon} />
-        <FooterButton
-          title="Give me a call"
-          icon={CallIcon}
-          link="tel:+916204789892"
-        />
+    <div className="relative h-56">
+      <div
+        id="slope"
+        className="h-2/3 before:absolute before:top-0 before:left-0 before:w-full before:h-2/3 before:bg-gradient-to-bl before:from-transparent before:from-50% before:to-[#211E1E] before:to-50%"
+      ></div>
+      <div className="h-full bg-[#211E1E]">
+        <h5 className="text-primary font-semibold text-2xl bottom-0 text-center pt-6 pb-12">
+          Thanks for visiting!
+        </h5>
+        <div className="flex flex-row justify-center gap-8">
+          <FooterButton
+            title="Back to top"
+            icon={UpArrowIcon}
+            onClick={handleBackToTopClick}
+          />
+          <FooterButton title="Send message" icon={MessageIcon} />
+          <FooterButton
+            title="Give me a call"
+            icon={CallIcon}
+            link="tel:+916204789892"
+          />
+        </div>
       </div>
     </div>
   );
