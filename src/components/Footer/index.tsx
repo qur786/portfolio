c7d57b0a6e1b6f5bd4aa45690d0c9bb6ae97adde
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-29 19:32:08 +0530

    feat: add three footer butons for back to top, sending message and for a call

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,13 @@
 export function Footer(): JSX.Element {
   return (
-    <div className="h-64">
+    <div className="h-72 pt-8">
       <h5 className="text-primary font-semibold text-2xl static bottom-0 text-center py-12">
         Thanks for visiting!
       </h5>
-      <div>
-        <div></div>
-        <div></div>
+      <div className="flex flex-row justify-center gap-8">
+        <button>Back to top</button>
+        <button>Send message</button>
+        <button>Give me a call</button>
       </div>
     </div>
   );
