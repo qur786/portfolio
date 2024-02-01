169640a896341cf16c85da8fb25a62c929d4863a
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-01 18:30:30 +0530

    refactor: make modal responvie width

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -12,7 +12,7 @@ export const Modal = forwardRef(
     return (
       <dialog
         ref={modalRef}
-        className="bg-[#36454F] text-white min-w-[30%] backdrop:backdrop-contrast-[0.25] rounded-lg"
+        className="bg-[#36454F] text-white md:min-w-[35%] min-w-[60%] backdrop:backdrop-contrast-[0.25] rounded-lg"
       >
         <button
           className="p-4 absolute top-0 right-0 text-red-600 hover:text-red-800 font-bold"
