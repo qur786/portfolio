07dce59d16dd052da52021c0dc52273fec4672c2
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-01 18:58:31 +0530

    refactor: fix project card responsiveness

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -21,7 +21,7 @@ export function Project({
   imageClipPosition = "0px_25%",
 }: ProjectProps): JSX.Element {
   return (
-    <div className="max-w-[280px] h-[440px] rounded-lg bg-[#222222]">
+    <div className="max-w-[320px] min-h-[440px] rounded-lg border bg-[#222222]">
       <img
         className={`w-full h-60 object-cover rounded-tl-lg rounded-tr-lg object-[${imageClipPosition}]`}
         src={image}
