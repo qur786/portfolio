d334b235ce9629626c7887d7a7cb8bc8b2ceb5e6
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-01 22:38:37 +0530

    refactor: add shadow to about card

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -11,7 +11,7 @@ export function About(): JSX.Element {
         alt="Profile"
         className="bg-contain w-48 h-48 relative md:left-16 md:-top-12 top-16 rounded-xl border-primary border-2"
       />
-      <div className="rounded-lg md:pl-20 md:pr-10 pb-6 md:pt-6 pt-16 px-4 border-primary border-2">
+      <div className="rounded-lg shadow-2xl md:pl-20 md:pr-10 pb-6 md:pt-6 pt-16 px-4 border-primary border-2">
         <h2 className="text-3xl text-primary font-bold py-2 md:text-start text-center">
           About me
         </h2>
