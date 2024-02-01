255fac11b11b895224f54969cde37993c624457a
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-01 22:19:42 +0530

    feat: added ThemeProvider to the app

--- src/main.tsx
+++ src/main.tsx
@@ -8,15 +8,18 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/700.css";
 import "@fontsource/roboto/900.css";
 import "./index.css";
+import { ThemeProvider } from "./context/theme-context.tsx";
 
 ReactGA.initialize("G-ZQW766DHW5");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MobileProvider>
-      <div className="dark:bg-[#121212] dark:text-white font-normal font-roboto">
-        <App />
-      </div>
+      <ThemeProvider>
+        <div className="dark:bg-[#121212] dark:text-white font-normal font-roboto">
+          <App />
+        </div>
+      </ThemeProvider>
     </MobileProvider>
   </StrictMode>
 );
