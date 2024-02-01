ae5eaa7698374a363156f44f8f58b26ee6bdd35c
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-01 21:58:32 +0530

    refactor: use as much tailwind css for exprience component

--- src/components/ExperienceItem/index.tsx
+++ src/components/ExperienceItem/index.tsx
@@ -28,11 +28,8 @@ export function ExperienceItem({
       contentStyle={{ background: "#222222" }}
       contentArrowStyle={{ borderRight: "7px solid #222222" }}
       date={date}
-      iconStyle={{
-        background: "#222222",
-        boxShadow:
-          "0 0 0 4px #FF020A, inset 0 2px 0 rgba(0, 0, 0, 0.08), 0 3px 0 4px rgba(0, 0, 0, 0.05)", // Icon container outer border
-      }}
+      dateClassName="dark:text-white"
+      iconClassName="bg-white dark:fill-white fill-black dark:bg-[#222222] shadow-experience-icon-border"
       icon={icon}
     >
       <div className="flex flex-col gap-2">
@@ -40,13 +37,11 @@ export function ExperienceItem({
         <div className="flex md:flex-row flex-col justify-between md:items-center">
           <div className="flex flex-row md:gap-x-0 gap-x-1 items-center">
             <CompanyIcon className="h-4" />
-            <h4 className="md:text-sm text-xs py-2 dark:bg:text-gray-200 font-light">
-              {company}
-            </h4>
+            <h4 className="md:text-sm text-xs py-2 font-light">{company}</h4>
           </div>
           <div className="flex flex-row md:gap-x-0 gap-x-1 items-center md:text-right text-left">
             <LocationIcon className="h-4" />
-            <h4 className="md:text-sm text-xs py-2 font-light dark:text-gray-200">
+            <h4 className="md:text-sm text-xs py-2 font-light">
               {city}, {country}
             </h4>
           </div>
