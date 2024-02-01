0320611a8e7054887b99a0bade4b82a1f8e83c3a
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-01 22:35:40 +0530

    fix: extends custom tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,18 @@ export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    boxShadow: {
-      "experience-icon-border":
-        "0 0 0 4px #FF020A, inset 0 2px 0 rgba(0, 0, 0, 0.08), 0 3px 0 4px rgba(0, 0, 0, 0.05)",
-      "experience-icon-border-success":
-        "0 0 0 4px #45CE30, inset 0 2px 0 rgba(0, 0, 0, 0.08), 0 3px 0 4px rgba(0, 0, 0, 0.05)",
-    },
-    fontFamily: {
-      roboto: ["Roboto", "sans-serif"],
-    },
     extend: {
       colors: {
         primary: "#FF020A",
+      },
+      boxShadow: {
+        "experience-icon-border":
+          "0 0 0 4px #FF020A, inset 0 2px 0 rgba(0, 0, 0, 0.08), 0 3px 0 4px rgba(0, 0, 0, 0.05)",
+        "experience-icon-border-success":
+          "0 0 0 4px #45CE30, inset 0 2px 0 rgba(0, 0, 0, 0.08), 0 3px 0 4px rgba(0, 0, 0, 0.05)",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
       },
     },
   },
