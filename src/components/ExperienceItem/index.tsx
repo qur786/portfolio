eaa5ed3a5d71c8d1a0d613db8649235f0f26f192
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-01 22:30:51 +0530

    feat: configure ExperienceItem for dark mode

--- src/components/ExperienceItem/index.tsx
+++ src/components/ExperienceItem/index.tsx
@@ -2,17 +2,21 @@ import { VerticalTimelineElement } from "react-vertical-timeline-component";
 import { ReactNode } from "react";
 import { CompanyIcon } from "../../Icons/company";
 import { LocationIcon } from "../../Icons/location";
+import type { Theme } from "../../context/utils";
+
+interface Location {
+  city: string;
+  country: string;
+}
 
 interface ExperienceItemProps {
   date: string;
   icon: ReactNode;
   role: string;
-  location: {
-    city: string;
-    country: string;
-  };
+  location: Location;
   work: string[];
   company: string;
+  theme?: Theme;
 }
 
 export function ExperienceItem({
@@ -22,25 +26,30 @@ export function ExperienceItem({
   location: { city, country },
   work,
   company,
+  theme,
 }: ExperienceItemProps): JSX.Element {
   return (
     <VerticalTimelineElement
-      contentStyle={{ background: "#222222" }}
-      contentArrowStyle={{ borderRight: "7px solid #222222" }}
+      contentStyle={{ background: theme === "dark" ? "#222222" : "#DAE0E2" }}
+      contentArrowStyle={{
+        borderRight:
+          theme === "dark" ? "7px solid #222222" : "7px solid #DAE0E2",
+      }}
       date={date}
       dateClassName="dark:text-white"
-      iconClassName="bg-white dark:bg-[#222222] dark:fill-white fill-black shadow-experience-icon-border"
+      iconClassName="bg-white dark:bg-[#222222] dark:fill-white fill-[#616C6F] shadow-experience-icon-border"
       icon={icon}
+      className="drop-shadow-2xl"
     >
       <div className="flex flex-col gap-2">
         <h3 className="text-lg font-bold md:text-start text-center">{role}</h3>
         <div className="flex md:flex-row flex-col justify-between md:items-center">
           <div className="flex flex-row md:gap-x-0 gap-x-1 items-center">
-            <CompanyIcon className="h-4" />
+            <CompanyIcon className="h-4 fill-[#218F76]" />
             <h4 className="md:text-sm text-xs py-2 font-light">{company}</h4>
           </div>
           <div className="flex flex-row md:gap-x-0 gap-x-1 items-center md:text-right text-left">
-            <LocationIcon className="h-4" />
+            <LocationIcon className="h-4 fill-[#EA4335]" />
             <h4 className="md:text-sm text-xs py-2 font-light">
               {city}, {country}
             </h4>
