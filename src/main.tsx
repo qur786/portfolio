0d41a150df777237724c836e9120513cd2d3bc07
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-02 10:39:04 +0530

    refactor: added roboto 400 and 500

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { MobileProvider } from "./context/mobile-context.tsx";
 import ReactGA from "react-ga4";
 import "@fontsource/roboto/100.css";
 import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "@fontsource/roboto/900.css";
 import "./index.css";
