39f9df9fcf72c80f423ea75a519174e820cddb29
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-21 20:02:26 +0530

    fix: fix star visibility issue

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { MessageForm, MessageFormProps } from "./components/MessageForm";
 import { ThemeContext } from "./context/theme-context";
 import { send } from "@emailjs/browser";
 import { useSnackbar } from "notistack";
+import { Stars } from "./components/Stars";
 
 export function App(): JSX.Element {
   const { theme, toggleTheme } = useContext<ThemeContext>(ThemeContext);
@@ -80,6 +81,9 @@ export function App(): JSX.Element {
 
   return (
     <main className="select-none" ref={mainRef}>
+      <div className="h-screen w-full fixed top-0 left-0">
+        <Stars />
+      </div>
       <SocialSidebar theme={theme} toggleTheme={toggleTheme} />
       <Fade triggerOnce duration={5000}>
         <Introduction onViewWorkClick={handleViewButtonClick} />
--- src/components/Introduction/index.tsx
+++ src/components/Introduction/index.tsx
@@ -1,4 +1,3 @@
-import { Stars } from "../Stars";
 import type { MouseEventHandler } from "react";
 
 export interface IntroductionProps {
@@ -9,24 +8,19 @@ export function Introduction({
   onViewWorkClick,
 }: IntroductionProps): JSX.Element {
   return (
-    <>
-      <div className="h-screen w-full fixed top-0 left-0">
-        <Stars />
-      </div>
-      <div className="h-screen relative flex justify-center items-center flex-col gap-6 font-mono">
-        <p className="text-5xl text-center">
-          Hello, I&apos;m <b className="text-red-600">Qurban</b>
-        </p>
-        <p className="text-5xl text-center">
-          A <b className="text-green-600">MERN</b> stack developer.
-        </p>
-        <button
-          className={`transition-transform text-lg font-roboto hover:scale-110 dark:text-white duration-300 font-bold bg-white shadow-xl md:shadow-2xl dark:bg-black-elevation-1 dark:shadow-black-shadow dark:shadow-inner rounded-md py-4 px-8`}
-          onClick={onViewWorkClick}
-        >
-          View my work &#x2193;
-        </button>
-      </div>
-    </>
+    <div className="h-screen relative flex justify-center items-center flex-col gap-6 font-mono">
+      <p className="text-5xl text-center">
+        Hello, I&apos;m <b className="text-red-600">Qurban</b>
+      </p>
+      <p className="text-5xl text-center">
+        A <b className="text-green-600">MERN</b> stack developer.
+      </p>
+      <button
+        className={`transition-transform text-lg font-roboto hover:scale-110 dark:text-white duration-300 font-bold bg-white shadow-xl md:shadow-2xl dark:bg-black-elevation-1 dark:shadow-black-shadow dark:shadow-inner rounded-md py-4 px-8`}
+        onClick={onViewWorkClick}
+      >
+        View my work &#x2193;
+      </button>
+    </div>
   );
 }
