142cc30851718eb1943ee241b096053c26a88678
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-03-21 19:48:02 +0530

    refactor: updated fe job description

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -39,10 +39,9 @@ export function Experience({ theme }: ExperienceProps): JSX.Element {
           company="Manufac Analytics Pvt Ltd"
           role="Front End Developer"
           work={[
-            "Engineered diverse projects: web apps, single-page apps, and Electron apps. Innovations include tracking user activity with Python and Electron, and real-time IoT tracking with AWS IoT, Geofencing, Lambda, and SNS.",
-            "Improved website performance by optimizing Lighthouse scores, achieving a 90+% score in Lighthouse.",
-            "Enhanced UI with mobile-responsive components, charts, and forms in React. Achieved a 30% improvement in UI responsiveness.",
-            "Developed and debugged the company's core product, API. Improving reliability by 10%.",
+            "Notable innovations include user activity tracking with Python and Electron, Bluetooth data reading via Web API, and real-time IoT tracking with AWS IoT, Geofencing, Lambda, and SNS.",
+            "Achieved a 90%+ score in Lighthouse by optimizing website performance.",
+            "Elevated UI standards through mobile-responsive components, charts, and forms in React (using Formik, D3.js, ECharts, ReactTable, and MUI), resulting in a 30% improvement in UI responsiveness and heightened user engagement.",
           ]}
         />
         <ExperienceItem
